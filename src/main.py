01285679bf79e13d868348ce8ae10786c901fa49
mwglen <mwg2202@yahoo.com>
2021-05-16 01:36:24 -0400

    Added classes representing a board and chess pieces

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import curses
+from enum import Enum, auto
 
 def main(stdscr):
     # Clear screen
@@ -13,6 +14,59 @@ def main(stdscr):
     stdscr.bkgd(" ", curses.color_pair(2))
     stdscr.box("|", "-")
     stdscr.addstr(0, 0, "Chess Application", curses.color_pair(1))
+    
+    board = Board()
+
     stdscr.getch()
+
+# A class representing a chess board
+class Board(dict):
+    def new(self) -> dict:
+        # Create a dictionary to map positions to pieces
+        board = {}
+
+        # The layout of the pieces on the first and eigth row
+        layout = [
+            PieceType.PAWN,
+            PieceType.KNIGHT,
+            PieceType.BISHOP,
+            PieceType.QUEEN,
+            PieceType.KING,
+            PieceType.BISHOP,
+            PieceType.KNIGHT,
+            PieceType.ROOK,
+        ]
+
+        # Position the pieces on the board
+        for (col, pt) in zip("abcdefgh", layout):
+            # Position the pawns
+            for (row, c) in zip("27", Color):
+                board[col + row] = Piece(c, PieceType.PAWN)
+            # Position the other pieces
+            for (row, c) in zip("18", Color):
+                board[col + row] = Piece(c, pt)
+
+        # Return the dictionary
+        return board
+
+# Types of pieces
+class PieceType(Enum):
+    PAWN = auto()
+    KNIGHT = auto()
+    BISHOP = auto()
+    ROOK = auto()
+    QUEEN = auto()
+    KING = auto()
+
+# Possible colors for a piece
+class Color(Enum):
+    WHITE = auto()
+    BLACK = auto()
+
+# A piece with a specified color
+class Piece:
+    def __init__(self, pt: PieceType, c: Color):
+        self.type = pt
+        self.color = c
 
 curses.wrapper(main)
