f750f7016f9652d1553175797c4e33a733b59ca6
mwglen <mwg2202@yahoo.com>
2021-05-16 14:49:30 -0400

    Added code to automatically choose the size of the chessboard, position it in the center of the terminal, and update on resize

--- src/main.py
+++ src/main.py
@@ -11,19 +11,32 @@ def main(w):
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_BLACK)
 
-    # Color the main window
-    w.bkgd(" ", curses.color_pair(1))
-    w.box("|", "-")
+   
+    y, x = w.getmaxyx()
+    while True:
+        # Resize terminal if needed
+        if not(curses.is_term_resized(x, y)):
+            y, x = w.getmaxyx()
+            w.clear()
+            curses.resizeterm(y, x)
+            w.refresh()
+        
+        # Color the main window
+        w.bkgd(" ", curses.color_pair(1))
+        w.box("|", "-")
 
-    # Add the title
-    w.addstr(0, 0, "Chess Application", curses.color_pair(1))
-   
-    # Build and draw the board
-    board = Board(w)
-    board.draw()
-   
-    # Wait for keypress
-    w.getch()
+        
+        # Add the title
+        w.addstr(0, 0, "Chess Application", curses.color_pair(1))
+
+        # Build and draw the board
+        board = Board(w)
+        board.draw()
+       
+        # Wait for keypress
+        c = w.getch()
+        w.erase()
+
 
 # A class representing a chess board
 class Position(dict):
@@ -62,18 +75,25 @@ class Board():
         self.position = Position.base()
         
         windows = {}
-        height = 3
-        width = 6
-        y = 10
-        x = 10
+        bs = 6 # The border size
+        max_y, max_x = base_window.getmaxyx()
+        if ((max_x - bs) / 8)*2 < (max_y - bs)/8:
+            width = int((max_x - bs) / 8)
+            height = int(width / 2)
+        else:
+            height = int((max_y - bs)/ 8)
+            width = height * 2
+
+        y = int((max_y - height*8)/2)
+        x = int((max_x - width*8)/2)
+
         for row in "12345678":
             for col in "abcdefgh":
                 w = base_window.subwin(height, width, y, x)
-                w.bkgd(row)
                 windows[col + row] = w
                 x += width
             y += height
-            x = 10
+            x = int((max_x - width*8)/2)
         self.windows = windows
 
     def draw(self):
@@ -93,7 +113,8 @@ class Board():
                 
                 # Draw the piece if needed
                 if piece:
-                    w.addch(0, 0, piece.type.as_letter())
+                    w.addch(0, 0, piece.type.as_letter(),
+                            curses.A_BOLD)
 
 # Types of pieces
 class PieceType(Enum):
